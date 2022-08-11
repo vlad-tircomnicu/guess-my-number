9858e415cbaddaf96fd4c505b659c7883e1f3304
vlad-tircomnicu <tircomnicu@gmail.com>
2022-08-11 21:41:51 +0300

    User number input logic
    Implement the logic of the user input value

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const
+    number = Math.trunc(Math.random() * 20) + 1
+
+let
+    check = document.querySelector('.check'),
+    message = document.querySelector('.message'),
+    hiddenNumber = document.querySelector('.number')
+
+check.addEventListener('click', () => {
+    const guess = Number( document.querySelector('.guess').value )
+
+    if (!guess) {
+        message.textContent = 'No number'
+    } else if (guess === number) {
+        message.textContent = 'Number is correct'
+        hiddenNumber.textContent = String(number)
+    } else if (guess > number) {
+        message.textContent = 'Number is to high'
+    } else {
+        message.textContent = 'Number is to low'
+    }
+})
