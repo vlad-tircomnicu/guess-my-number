f742304c26d75821bbea0bb0a42058314c613e84
vlad-tircomnicu <tircomnicu@gmail.com>
2022-08-16 17:37:46 +0300

    Transform message into a function
    Refactor the code for the message displayed to the user into a function.

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 
 const
     check = document.querySelector('.check'),
-    message = document.querySelector('.message'),
     hiddenNumber = document.querySelector('.number'),
     highScore = document.querySelector('.highscore'),
     score = document.querySelector('.score'),
     body = document.querySelector('body'),
     again = document.querySelector('.again'),
-    guessInput = document.querySelector('.guess')
+    guessInput = document.querySelector('.guess'),
+    displayMessage = function(message) {
+        document.querySelector('.message').textContent = message
+    }
 
 let
     secretNumber = Math.trunc(Math.random() * 20) + 1,
@@ -20,23 +22,19 @@ check.addEventListener('click', () => {
 
     // if no number is submitted
     if (!guess) {
-        message.textContent = 'No number'
+        displayMessage('No number')
     // if the number is incorrect
-    } else if (guess > secretNumber || guess < secretNumber) {
+    } else if (guess !== secretNumber) {
         if (liveScore > 1) {
             liveScore--
-            if (guess > secretNumber) {
-                message.textContent = 'Number is to high'
-            } else {
-                message.textContent = 'Number is to low'
-            }
+            displayMessage(guess > secretNumber ? ('To high') : ('To low'))
         } else {
-            message.textContent = 'You lost the game'
+            displayMessage('You lost the game')
             liveScore = 0
         }
     // if the number is correct
     } else {
-        message.textContent = 'Number is correct'
+        displayMessage('Number is correct')
         hiddenNumber.textContent = String(guess)
         body.style.backgroundColor = '#60b347'
         if (liveHighScore < liveScore) {
@@ -52,7 +50,7 @@ check.addEventListener('click', () => {
 again.addEventListener('click', () => {
     liveScore = 20
     score.textContent = liveScore
-    message.textContent = 'Start guessing...'
+    displayMessage('Start guessing')
     body.style.backgroundColor = '#222'
     hiddenNumber.textContent = '?'
     secretNumber = Math.trunc(Math.random() * 20) + 1
